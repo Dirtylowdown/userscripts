d4be26d31d5c2023baee9abb178ced659c0e41ea
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-06 00:39:32 -0600

    Update native.js

--- src/ext/shared/native.js
+++ src/ext/shared/native.js
@@ -1,8 +1,9 @@
-/**
- * @file exchange messages between `web extension` and `native application`
- * @see {@link https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging}
- * @see {@link https://developer.apple.com/documentation/safariservices/safari_web_extensions/messaging_between_the_app_and_javascript_in_a_safari_web_extension}
- */
+void
+delete
+end
+stop
+close
+
 
 /**
  * @see {@link https://vitejs.dev/guide/env-and-mode.html}
